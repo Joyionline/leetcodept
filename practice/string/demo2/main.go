12e228dee5d982758847916e5144adde04efa3e5
Joyionline <21023453+Joyionline@users.noreply.github.com>
2019-05-31 09:24:30 +0800

    Today's practice

--- practice/string/demo2/main.go
+++ practice/string/demo2/main.go
@@ -8,7 +8,9 @@ import (
 
 func main() {
 	// demo2_countSegments_main()
-	demo2_compress_main()
+	// demo2_compress_main()
+	// demo2_longestCommonPrefix_main()
+	firstUniqChar_main()
 }
 
 /*
@@ -77,4 +79,95 @@ func compress(chars []byte) int {
 	}
 	fmt.Println("当前字符串：", string(chars))
 	return len(chars)
+}
+
+/*
+	最长公共前缀
+	编写一个函数来查找字符串数组中的最长公共前缀。
+	如果不存在公共前缀，返回空字符串 ""。
+	示例 1:
+	输入: ["flower","flow","flight"]
+	输出: "fl"
+	示例 2:
+	输入: ["dog","racecar","car"]
+	输出: ""
+	解释: 输入不存在公共前缀。
+*/
+func demo2_longestCommonPrefix_main() {
+	strarr := []string{"flower", "flow", "flight"}
+	fmt.Println("该字符串数组中最长的公共前缀是：", longestCommonPrefix(strarr))
+
+	// str := "hello"
+	// fmt.Println(strings.Index(str, "e"))
+
+}
+
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+	var comstr string = strs[0]
+	for i := 1; i < len(strs); i++ {
+		for strings.Index(strs[i], comstr) != 0 {
+			comstr = comstr[0 : len(comstr)-1]
+			if comstr != "" {
+				return ""
+			}
+		}
+		fmt.Println("截取数据：", comstr)
+	}
+	return comstr
+}
+
+/*
+	字符串中的第一个唯一字符
+	给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
+	案例:
+	s = "leetcode"
+	返回 0.
+	s = "loveleetcode",
+	返回 2.
+*/
+
+func firstUniqChar_main() {
+	// 测试用例  "leetcode"  | "loveleetcode" | "oooooo" | "dddccdbba" | "cc"
+	s := "dddccdbba"
+	fmt.Println("当前字符串中第一个唯一字符索引是：", firstUniqChar_2(s))
+}
+
+// 暴力解法超时
+func firstUniqChar(s string) int {
+	var count int
+	var laststr string
+	for i := 0; i < len(s); i++ {
+		if string(s[i]) != laststr {
+			count = 0
+		}
+		for j := 0; j < len(s); j++ {
+			if s[i] == s[j] {
+				count++
+			}
+			if count > 1 {
+				continue
+			}
+			if j == len(s)-1 && count == 1 {
+				return i
+			}
+		}
+	}
+	return -1
+}
+
+// map 实现
+func firstUniqChar_2(s string) int {
+	h := map[rune]int{}
+	for _, v := range s {
+		h[v]++
+	}
+	for k, v := range s {
+		if h[v] == 1 {
+			return k
+		}
+	}
+	return -1
 }
